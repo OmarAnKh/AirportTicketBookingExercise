79d726a756bccac2f7b0e2b65df2e0b432b01a9b
Omar Khalili <OmarKhalili810@gmail.com>
2025-01-01 15:16:46 +0200

    adding the Newtonsoft.Json

--- AirportTicketBookingExercise/obj/Debug/net8.0/AirportTicketBookingExercise.AssemblyInfo.cs
+++ AirportTicketBookingExercise/obj/Debug/net8.0/AirportTicketBookingExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirportTicketBookingExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90f615782ee69c347557c734863f2b01f1faef04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45dea0ffca632678330125bdeffa8b0361f6594f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirportTicketBookingExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirportTicketBookingExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
